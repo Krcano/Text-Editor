6fd25e34b1c094d95b6bcb47476950b1e73b5dcc
Kelly R Cano <krcano2001@gmail.com>
2022-01-05 21:45:28 -0800

    fixed code with prettier by adding dbl quotes and correct spacing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({id:1, value:content});
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
@@ -41,6 +41,5 @@ export const getDb = async () => {
   const result = await request;
   console.log("result.value", result);
   return result?.value;
-
 };
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,34 +1,34 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // does this make the install button appear by default?
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false)
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
 });
 
 // When the install button is clicked does this make the button hide?
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
-   return;
+    return;
   }
 
   // Show prompt
   promptEvent.prompt();
-  
+
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+
+  butInstall.classList.toggle("hidden", true);
 });
 
 // Exactly how does the appinstalled event work?
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-     // Clear prompt
+window.addEventListener("appinstalled", (event) => {
+  // Clear prompt
   window.deferredPrompt = null;
 });
